fbcd98c305c51fa48e00489f4ec93381da7e9462
enigma9119 <enigma9119@users.noreply.github.com>
2023-09-03 01:01:54 -0700

    Add ability to do calculations
    Listen for click events and execute various math operations as per user
    input. Round off decimal values to 2 places. Clear button removes all
    text from the display and delete button removes only the last value.

--- script.js
+++ script.js
@@ -18,19 +18,120 @@ function modulo(num1, num2) {
   return num1 % num2;
 }
 
+function round(num, precision) {
+  let multiplier = 10 ** precision;
+  return Math.round(num * multiplier) / multiplier;
+}
+
 function operate(num1, num2, operator) {
+  let result;
   switch (operator) {
     case "+":
-      return add(num1, num2);
+      result = add(num1, num2);
+      break;
     case "-":
-      return subtract(num1, num2);
-    case "*":
-      return multiply(num1, num2);
+      result = subtract(num1, num2);
+      break;
+    case "x":
+      result = multiply(num1, num2);
+      break;
     case "/":
-      return divide(num1, num2);
+      result = divide(num1, num2);
+      break;
     case "%":
-      return modulo(num1, num2);
+      result = modulo(num1, num2);
+      break;
+  }
+
+  return round(result, 2);
+}
+
+function populateDisplay(text) {
+  const display = document.querySelector(".display");
+  display.textContent += text;
+}
+
+function clearDisplay() {
+  const display = document.querySelector(".display");
+  display.textContent = "";
+}
+
+function deleteFromDisplay() {
+  const displayText = document.querySelector(".display").textContent;
+
+  clearDisplay();
+  populateDisplay(displayText.slice(0, -1));
+
+  if (state === "num1") {
+    num1 = Math.round(num1 / 10);
+  } else {
+    num2 = Math.round(num2 / 10);
+  }
+
+  if (Number.isNaN(+displayText[-1])) {
+    state = "num1";
   }
 }
 
-let num1, num2, operator;
+function updateNumber(digit, state) {
+  populateDisplay(digit);
+
+  if (state === "num1") {
+    num1 = num1 * 10 + digit;
+  } else {
+    num2 = num2 * 10 + digit;
+  }
+}
+
+function execute(e) {
+  const classListArray = Array.from(e.target.classList);
+  if (
+    classListArray.includes("delete") ||
+    classListArray.includes("fa-delete-left")
+  ) {
+    deleteFromDisplay();
+    return;
+  }
+
+  let buttonText = e.target.textContent;
+  if (classListArray.includes("divide")) buttonText = "/";
+
+  if (buttonText === "AC") {
+    clearDisplay();
+    [num1, num2, operator] = [0, 0, ""];
+    state = "empty";
+  } else if (state == "empty") {
+    if (+buttonText >= 0 && +buttonText <= 9) {
+      state = "num1";
+      clearDisplay();
+      updateNumber(+buttonText, state);
+    }
+  } else if (+buttonText >= 0 && +buttonText <= 9) {
+    updateNumber(+buttonText, state);
+  } else if (state === "num1") {
+    operator = buttonText;
+    populateDisplay(operator);
+    state = "num2";
+  } else if (num2 && state === "num2") {
+    let result = operate(num1, num2, operator);
+    [num1, num2] = [result, 0];
+
+    if (buttonText != "=") {
+      operator = buttonText;
+      result += operator;
+    } else {
+      operator = "";
+      state = "empty";
+      num1 = 0;
+    }
+
+    clearDisplay();
+    populateDisplay(result);
+  }
+}
+
+let [num1, num2, operator] = [0, 0, ""];
+let state = "empty";
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", execute));
